adf8f615bf655442e78142c894d3a413ae7c65e9
qiusha <qiusha.zhu@gmail.com>
2015-02-26 12:27:03 -0500

    testing

--- src/main/groovy/com/senzari/psl/MatchArtistTest.groovy
+++ src/main/groovy/com/senzari/psl/MatchArtistTest.groovy
@@ -81,7 +81,7 @@ m.add function: "sameYear" , implementation: new YearSimilarity()
  */
 //m.add rule : ( artistName(A,AName) & artistName(B,BName) & (A ^ B) & sameName(AName,BName) ) >> sameArtist(A,B),  weight : 5
 
-m.add rule : ( trackTitle(A,AName) & trackTitle(B,BName) & (A ^ B) & sameName(AName,BName)) >> sameTrack(A,B),  weight : 8
+m.add rule : ( trackTitle(A,AName) & trackTitle(B,BName) & (A ^ B) & sameName(AName,BName)) >> sameTrack(A,B),  weight : 50
 m.add rule : ( trackYear(A,AYear) & trackYear(B,BYear) & sameYear(AYear,BYear)) >> sameTrack(A,B),  weight : 3
 m.add rule : (trackArtist(A, AArtist) & trackArtist(B, BArtist) & (A ^ B) & sameName(AArtist, BArtist)) >> sameTrack(A,B),  weight : 5
 m.add rule : (trackAlbum(A, AAlbum) & trackAlbum(B, BAlbum) & (A ^ B) & sameName(AAlbum, BAlbum)) >> sameTrack(A,B),  weight : 3
